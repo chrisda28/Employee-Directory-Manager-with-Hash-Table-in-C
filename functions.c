8ea65d80b4f1d7c07515e30b08cf324685b3c451
chrisda28 <chrisda28@gmail.com>
2024-07-01 18:27:00 -0400

    finish function that inserts new employee into hash table and implements chaining to prevent clustering

--- functions.c
+++ functions.c
@@ -6,41 +6,52 @@
 #include <strings.h>
 #include <functionality.h>
 
-
-void initialize_table();
-{// sets each bucket of hash table to NULL
+// sets each bucket of hash table to NULL
+void initialize_table()
+{
     for (int i = 0; i < HASH_MAX; i++)
-        {
             table[i] = NULL;   // indicate that no employees have been recorded yet
-        }
-
 }
 
 
 int index hash(int id)
-{
     return id % HASH_MAX;  // taking remainder of ID divided by HASH_MAX
+
+void insert_employee(employee emp)
+{
+    int hash_index = hash(emp.id);  // finds hash index
+    table[hash_index] = emp;
+    node *new_node = malloc(sizeof(node));
+    if (new_node == NULL)  // error handling
+        {
+            printf("Memory related error\n");
+            return NULL;
+        }
+    if (table[hash_index] != NULL)  // prepend new_node to beginning of linked list
+    {
+        
+    }
+    else
+        table[hash_index] = new_node;  // directly assign
+
+
+
 }
 
-void insert_employee(employee emp);
+
+employee *find_employee(int id)
 {
 
 }
 
 
-employee *find_employee(int id);
+void delete_employee(int id)
 {
 
 }
 
 
-void delete_employee(int id);
-{
-
-}
-
-
-void print_table();
+void print_table()
 {
 
 }
